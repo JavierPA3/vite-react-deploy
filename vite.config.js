da2137c09a892218d8ed920f144ccd53c4c1a696
JavierPA3 <a21poarja@iesgrancapitan.org>
2024-12-02 14:58:43 +0100

    hola4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Esta es la carpeta donde Vite coloca los archivos de producción
+    sourcemap: true, // Opcional: generar un archivo de mapa fuente, útil para depuración
+  }
+});
